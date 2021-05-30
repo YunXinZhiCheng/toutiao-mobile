b30a103cf1ccf76578f936fde7e967b91324ed33
罗远成 <57541209+YunXinZhiCheng@users.noreply.github.com>
2021-05-31 02:33:48 +0800

    登录注册-页面样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,28 +1,42 @@
 <template>
   <div class="login-container">
-    <van-nav-bar title="登录" left-arrow @click-left="$router.back()" />
+    <!-- 导航栏 -->
+    <van-nav-bar
+      class="app-nav-bar"
+      title="注册 / 登录"
+      left-arrow
+      @click-left="$router.back()"
+    />
+    <!-- /导航栏 -->
+
     <!-- 登录表单 -->
     <van-cell-group>
       <van-field
         v-model="user.mobile"
-        left-icon="smile-o"
+        icon-prefix="toutiao"
+        left-icon="shouji"
         placeholder="请输入手机号"
       />
       <van-field
         v-model="user.code"
         clearable
-        left-icon="music-o"
+        icon-prefix="toutiao"
+        left-icon="yanzhengma"
         placeholder="请输入验证码"
       >
         <!-- 验证码按钮 -->
         <template #button>
-          <van-button size="small" round>发送验证码</van-button>
+          <van-button class="send-btn" size="mini" round
+            >发送验证码
+          </van-button>
         </template>
       </van-field>
     </van-cell-group>
-    <!-- 登录按钮 -->
-    <van-button type="info" block>登录</van-button>
 
+    <div class="login-btn-wrap">
+      <!-- 登录按钮 -->
+      <van-button class="login-btn" type="info" block>登录</van-button>
+    </div>
     <!-- /登录表单 -->
   </div>
 </template>
@@ -48,4 +62,26 @@ export default {
 }
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+.login-container {
+  .send-btn {
+    width: 76px;
+    height: 23px;
+    background-color: #ededed;
+    .van-button__text {
+      font-size: 11px;
+      color: #666666;
+    }
+  }
+  .login-btn-wrap {
+    padding: 26px 16px;
+    .login-btn {
+      background-color: #6db4fb;
+      border: none;
+      .van-button__text {
+        font-size: 15px;
+      }
+    }
+  }
+}
+</style>
